f212a75ceb0bd2702b9c19db49ca2d8f7cf2d041
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-01 22:59:16 +0100

    refactor

--- day1/p2/main.cpp
+++ day1/p2/main.cpp
@@ -8,60 +8,15 @@
 #include <cstdlib>
 
 #define INPUT "input.txt"
-#define ERROR 1
+
+std::optional<short> get_digit(const char* s);
+std::optional<short> get_first_digit(const std::string& s);
+std::optional<short> get_last_digit(const std::string& s);
+short opt_char_to_int(std::optional<char> c);
 
 const std::array<const char*, 9> WORD_DIGITS = {
     "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-
-std::optional<short> get_digit(const char* s)
-{
-    if (isdigit(*s))
-        return *s - '0';
-    for (size_t i = 1; i < 10; i++)
-    {
-        const char* digit = WORD_DIGITS.at(i - 1);
-        if (!strncmp(s, digit, strlen(digit)))
-        {
-            return i;
-        }
-    }
-    return {};
-}
-
-const char* digit_to_word(short n)
-{
-    return WORD_DIGITS.at(n - 1);
-}
-
-std::optional<short> get_first(const std::string& s)
-{
-    std::optional<short> digit;
-    const char* s__ = s.c_str();
-
-    for (size_t i = 0; i < s.length(); i++)
-    {
-        digit = get_digit(s__ + i);
-        if (digit)
-            return digit;
-    }
-    return {};
-}
-
-std::optional<char> get_last(const std::string& s)
-{
-    std::optional<short> digit;
-    const char* s__ = s.c_str();
-
-    for (int i = s.length() - 1; i >= 0; i--)
-    {
-        digit = get_digit(s__ + i);
-        if (digit)
-            return digit;
-    }
-    return {};
-}
-
-short opt_char_to_int(std::optional<char> c);
+const char* digit_to_word(short n);
 
 int main()
 {
@@ -75,18 +30,62 @@ int main()
     std::string line;
     while (infile >> line)
     {
-        first_digit = get_first(line);
-        last_digit = get_last(line);
+        first_digit = get_first_digit(line);
+        last_digit = get_last_digit(line);
         if (!first_digit)
-            return ERROR;
+            break;
 
-        short n = 10 * *first_digit + *last_digit;
-
-        std::cout << line << '\n';
-        std::cout << *first_digit << '\t' << *last_digit << '\t' << n << '\n';
-
-        sum += n;
+        sum += 10 * first_digit.value() + last_digit.value();
     }
 
     std::cout << sum;
+}
+
+std::optional<short> get_digit(const char* s)
+{
+    if (isdigit(*s))
+        return *s - '0';
+
+    for (size_t digit = 1; digit < 10; digit++)
+    {
+        const char* digit_word = digit_to_word(digit);
+        if (strncmp(s, digit_word, strlen(digit_word)) == 0)
+        {
+            return digit;
+        }
+    }
+    return {};
+}
+
+std::optional<short> get_first_digit(const std::string& s)
+{
+    std::optional<short> digit;
+    const char* s__ = s.c_str();
+
+    for (size_t i = 0; i < s.length(); i++)
+    {
+        digit = get_digit(s__ + i);
+        if (digit)
+            return digit;
+    }
+    return {};
+}
+
+std::optional<short> get_last_digit(const std::string& s)
+{
+    std::optional<short> digit;
+    const char* s__ = s.c_str();
+
+    for (int i = s.length() - 1; i >= 0; i--)
+    {
+        digit = get_digit(s__ + i);
+        if (digit)
+            return digit;
+    }
+    return {};
+}
+
+const char* digit_to_word(short n)
+{
+    return WORD_DIGITS.at(n - 1);
 }
