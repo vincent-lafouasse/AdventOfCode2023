b007922af500a1f0582dcaccab0cd124357e6693
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-01 23:06:18 +0100

    seed day2

---
+++ day2/p1/main.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <iostream>
+
+#define INPUT "input.txt"
+
+int main()
+{
+    std::ifstream infile{INPUT};
+
+    std::string line;
+    while (infile >> line)
+    {
+		std::cout << line;
+    }
+}
