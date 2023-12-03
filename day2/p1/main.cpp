f310eec1b17abd4a2c9375df231160009c2cd7bb
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-03 22:25:42 +0100

    refactor

--- day2/p1/main.cpp
+++ day2/p1/main.cpp
@@ -1,102 +1,74 @@
-#include <array>
 #include <fstream>
 #include <iostream>
 #include <sstream>
-#include <string>
+#include <unordered_map>
 
-#define INPUT "input.txt"
-
-typedef std::array<int, 3> Hand;
-enum Color
-{
-    RED,
-    GREEN,
-    BLUE
+const std::unordered_map<std::string, int> MAX = {
+    {"red", 12},
+    {"green", 13},
+    {"blue", 14},
 };
 
-const Hand MAX = {12, 13, 14};
-
-bool hand_is_possible(const Hand& hand, const Hand& MAX)
+static size_t n_digits(unsigned n)
 {
-    for (size_t i = 0; i < hand.size(); i++)
+    size_t n_digits{};
+
+    while (n)
     {
-        if (hand[i] > MAX[i])
+        n /= 10;
+        n_digits++;
+    }
+    return n_digits;
+}
+
+static bool hand_is_valid(const std::string& hand)
+{
+    std::stringstream stream{hand};
+
+    std::unordered_map<std::string, int> colors;
+
+    char color[1024];
+    int n;
+
+    std::string draw;
+    while (std::getline(stream, draw, ','))
+    {
+        std::sscanf(draw.c_str(), " %i %s", &n, color);
+        std::string color_s = color;
+        colors[color_s] += n;
+        if (colors[color_s] > MAX.at(color_s))
             return false;
     }
     return true;
 }
 
-bool hand_is_valid(const std::string& hand_str)
+static bool game_is_valid(const std::string& game)
 {
-    std::stringstream hand_stream(hand_str);
-    std::string buffer;
-    Hand hand{};
+    std::stringstream stream{game};
 
-    int n;
-    std::string color;
-    std::stringstream stream;
-
-    while (getline(hand_stream, buffer, ','))
+    std::string hand;
+    while (std::getline(stream, hand, ';'))
     {
-        stream = std::stringstream(buffer);
-        stream >> n;
-        stream >> color;
-        if (color == "red")
-            hand[RED]++;
-        else if (color == "green")
-            hand[GREEN]++;
-        else if (color == "blue")
-            hand[BLUE]++;
-        else
+        if (!hand_is_valid(hand))
             return false;
     }
 
-    return hand_is_possible(hand, MAX);
+    return true;
 }
-
-class Game
-{
-   public:
-    Game() : id(0), valid(false) {}
-    Game(const std::string& line)
-    {
-        std::stringstream stream(line);
-        std::string garbage;
-
-        stream >> garbage;
-        stream >> id;
-        stream >> garbage;
-
-        std::string hand_str;
-        while (getline(stream, hand_str, ';'))
-        {
-            if (!hand_is_valid(hand_str))
-            {
-                valid = false;
-                return;
-            }
-        }
-        valid = true;
-    }
-
-    int id;
-    bool valid;
-};
 
 int main()
 {
-    std::ifstream infile{INPUT};
+    std::ifstream infile{"input.txt"};
 
     uint64_t sum{};
 
-    Game current_game;
-
-    std::string line;
-    while (getline(infile, line))
+    std::string game;
+    uint64_t id;
+    while (getline(infile, game))
     {
-        current_game = Game(line);
-        if (current_game.valid)
-            sum += current_game.id;
+        id = std::atoi(game.c_str() + 4);
+        if (game_is_valid(game.substr(7 + n_digits(id))))
+            sum += id;
     }
 
     std::cout << sum << '\n';
