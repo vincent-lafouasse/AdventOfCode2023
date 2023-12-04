cd5decb136ceda205a5bf0e2f916d3cd2d439dd4
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-04 19:12:55 +0100

    minor refactor

--- day2/p2/main.cpp
+++ day2/p2/main.cpp
@@ -4,6 +4,52 @@
 #include <unordered_map>
 #include <vector>
 
+typedef std::vector<std::string> StrVector;
+
+StrVector split(std::ifstream& stream, char sep);
+StrVector split(const std::string& s, char sep);
+int power_of_cube_set(const std::unordered_map<std::string, int>& set);
+std::unordered_map<std::string, int> init_cube_set(void);
+
+int main()
+{
+    std::ifstream infile{"input.txt"};
+
+    uint64_t sum{};
+
+    StrVector games = split(infile, '\n');
+    for (const std::string& game : games)
+    {
+        std::cout << game << '\n';
+    }
+
+    std::cout << sum << '\n';
+}
+
+StrVector split(std::ifstream& stream, char sep)
+{
+    StrVector out{};
+
+    std::string token;
+    while (std::getline(stream, token, sep))
+    {
+        out.push_back(token);
+    }
+    return out;
+}
+
+StrVector split(const std::string& s, char sep)
+{
+    StrVector out{};
+
+    std::stringstream stream{s};
+    std::string token;
+    while (std::getline(stream, token, sep))
+    {
+        out.push_back(token);
+    }
+    return out;
+}
 
 int power_of_cube_set(const std::unordered_map<std::string, int>& set)
 {
@@ -12,39 +58,10 @@ int power_of_cube_set(const std::unordered_map<std::string, int>& set)
 
 std::unordered_map<std::string, int> init_cube_set(void)
 {
-	std::unordered_map<std::string, int> cube_set;
-	cube_set["green"] = 0;
-	cube_set["blue"] = 0;
-	cube_set["red"] = 0;
+    std::unordered_map<std::string, int> cube_set;
+    cube_set["green"] = 0;
+    cube_set["blue"] = 0;
+    cube_set["red"] = 0;
 
-	return cube_set;
-}
-
-std::vector<std::string> split(const std::string& s, char sep)
-{
-	std::vector<std::string> out{};
-
-	std::stringstream stream{s};
-	std::string token;
-	while (std::getline(stream, token, sep))
-	{
-		out.push_back(token);
-	}
-	return out;
-}
-
-int main()
-{
-    std::ifstream infile{"input.txt"};
-
-    uint64_t sum{};
-
-    std::string game;
-    while (getline(infile, game))
-    {
-		std::cout << game.substr(game.find(": ") + 2) << '\n';
-		sum += power_of_cube_set(game.substr(game.find(": ") + 2));
-    }
-
-    std::cout << sum << '\n';
+    return cube_set;
 }
