1bd354e01d377e34dafdf317bf814a7e9995f2c2
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-05 14:23:51 +0100

    bug fix on edge of board

--- day3/p1/main.cpp
+++ day3/p1/main.cpp
@@ -17,34 +17,10 @@ bool is_symbol(char c);
 struct Position {
     Position(){};
     Position(i64 row, i64 col) : col(col), row(row) {}
-    char in(const std::vector<std::string>& lines) {
-        return lines.at(row).at(col);
-    }
-    bool is_valid(const std::vector<std::string>& lines) {
-        if (row < 0 || static_cast<u32>(row) == lines.size())
-            return false;
-        if (col < 0 || static_cast<u32>(col) == lines.at(row).size())
-            return false;
-        return true;
-    }
-    bool contains_symbol(const std::vector<std::string>& lines) {
-        return is_symbol(in(lines));
-    }
-    std::vector<Position> neighbours(const std::vector<std::string>& lines) {
-        const std::array<i64, 2> dXs = {-1, 1};
-        const std::array<i64, 2> dYs = {-1, 1};
-
-        std::vector<Position> out;
-
-        for (i64 dX : dXs) {
-            for (i64 dY : dYs) {
-                Position candidate{row + dX, col + dY};
-                if (candidate.is_valid(lines))
-                    out.push_back(candidate);
-            }
-        }
-        return out;
-    }
+    char in(const std::vector<std::string>& lines);
+    bool is_valid(const std::vector<std::string>& lines);
+    bool contains_symbol(const std::vector<std::string>& lines);
+    std::vector<Position> neighbours(const std::vector<std::string>& lines);
 
     i64 col;
     i64 row;
@@ -63,7 +39,8 @@ int main(void) {
             if (!start && std::isdigit(pos.in(lines))) {
                 start = col;
             }
-            if (start && !std::isdigit(pos.in(lines))) {
+            if (start && (!std::isdigit(pos.in(lines)) ||
+                          col == lines[row].size() - 1)) {
                 std::cout << std::atoi(lines.at(row).c_str() + *start) << '\n';
                 start = {};
             }
@@ -90,4 +67,34 @@ std::vector<std::string> split(std::ifstream& stream, char sep) {
 
 bool is_symbol(char c) {
     return !(std::isdigit(c) || c == EMPTY);
+}
+
+char Position::in(const std::vector<std::string>& lines) {
+    return lines.at(row).at(col);
+}
+bool Position::is_valid(const std::vector<std::string>& lines) {
+    if (row < 0 || static_cast<u32>(row) == lines.size())
+        return false;
+    if (col < 0 || static_cast<u32>(col) == lines.at(row).size())
+        return false;
+    return true;
+}
+bool Position::contains_symbol(const std::vector<std::string>& lines) {
+    return is_symbol(in(lines));
+}
+std::vector<Position> Position::neighbours(
+    const std::vector<std::string>& lines) {
+    const std::array<i64, 2> dXs = {-1, 1};
+    const std::array<i64, 2> dYs = {-1, 1};
+
+    std::vector<Position> out;
+
+    for (i64 dX : dXs) {
+        for (i64 dY : dYs) {
+            Position candidate{row + dX, col + dY};
+            if (candidate.is_valid(lines))
+                out.push_back(candidate);
+        }
+    }
+    return out;
 }
