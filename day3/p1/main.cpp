78c1a511a7f924da75b343d3635c679df406c8b3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-05 14:47:28 +0100

    refactor

--- day3/p1/main.cpp
+++ day3/p1/main.cpp
@@ -19,10 +19,10 @@ bool is_symbol(char c);
 struct Position {
     Position(){};
     Position(i64 row, i64 col) : col(col), row(row) {}
-    char in(const Lines& lines);
-    bool is_valid(const Lines& lines);
-    bool contains_symbol(const Lines& lines);
-    std::vector<Position> neighbours(const Lines& lines);
+    char in(const Lines& lines) const;
+    bool is_valid(const Lines& lines) const;
+    bool contains_symbol(const Lines& lines) const;
+    std::vector<Position> neighbours(const Lines& lines) const;
 
     i64 col;
     i64 row;
@@ -34,7 +34,43 @@ struct Number {
         std::string line = lines.at(pos.row);
         data = std::atoi(line.c_str() + pos.col);
     }
-    void log(void) { std::cout << data << '\n'; }
+    std::vector<Position> neighbours(const Lines& lines) {
+        std::vector<Position> out;
+        const size_t len = std::to_string(data).length();
+        Position candidate_above;
+        Position candidate_below;
+
+        for (size_t i = -1; i <= len; i++) {
+            candidate_above = Position(pos.row - 1, pos.col + i);
+            candidate_below = Position(pos.row + 1, pos.col + i);
+            if (candidate_above.is_valid(lines))
+                out.push_back(candidate_above);
+            if (candidate_below.is_valid(lines))
+                out.push_back(candidate_below);
+        }
+        Position candidate_before = Position(pos.row, pos.col - 1);
+        Position candidate_after = Position(pos.row, pos.col + len);
+        if (candidate_before.is_valid(lines))
+            out.push_back(candidate_before);
+        if (candidate_after.is_valid(lines))
+            out.push_back(candidate_after);
+
+        return out;
+    }
+
+    bool is_part(const Lines& lines) {
+        std::vector<Position> __neighbours = neighbours(lines);
+
+        for (const Position& neighbour : __neighbours) {
+            if (neighbour.contains_symbol(lines))
+                return false;
+        }
+        return true;
+    }
+
+    void log(const Lines& lines) {
+        std::cout << data << " is part : " << is_part(lines) << '\n';
+    }
 
     Position pos;
     i64 data;
@@ -56,7 +92,7 @@ int main(void) {
             if (start && (!std::isdigit(pos.in(lines)) ||
                           col == lines[row].size() - 1)) {
                 Number n({row, start.value()}, lines);
-                n.log();
+                n.log(lines);
                 start = {};
             }
         }
@@ -84,20 +120,20 @@ bool is_symbol(char c) {
     return !(std::isdigit(c) || c == EMPTY);
 }
 
-char Position::in(const Lines& lines) {
+char Position::in(const Lines& lines) const {
     return lines.at(row).at(col);
 }
-bool Position::is_valid(const Lines& lines) {
+bool Position::is_valid(const Lines& lines) const {
     if (row < 0 || static_cast<u32>(row) == lines.size())
         return false;
     if (col < 0 || static_cast<u32>(col) == lines.at(row).size())
         return false;
     return true;
 }
-bool Position::contains_symbol(const Lines& lines) {
+bool Position::contains_symbol(const Lines& lines) const {
     return is_symbol(in(lines));
 }
-std::vector<Position> Position::neighbours(const Lines& lines) {
+std::vector<Position> Position::neighbours(const Lines& lines) const {
     const std::array<i64, 2> dXs = {-1, 1};
     const std::array<i64, 2> dYs = {-1, 1};
 
